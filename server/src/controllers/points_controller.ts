2318e7d4dc649deb9df0d7e1172590f1d9f5d54e
Raul Barros <raulhbarros@outlook.com>
2023-09-25 15:53:26 +0100

    Cleaned tables and added generic url for image at create point function

--- server/src/controllers/points_controller.ts
+++ server/src/controllers/points_controller.ts
@@ -56,7 +56,7 @@ class PointsController {
       
           
           const point = {
-            image: 'image fake',
+            image: 'https://images.unsplash.com/photo-1526399743290-f73cb4022f48?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
             name,
             email,
             whatsapp,
